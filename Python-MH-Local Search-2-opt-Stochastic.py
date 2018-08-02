9403d1d23690486e55aedfa4833dafe43be16bb6
Valdecy Pereira <valdecypereira@yahoo.com.br>
2018-08-02 13:11:45 -0300

    Update Python-MH-Local Search-2-opt-Stochastic.py

--- Python-MH-Local Search-2-opt-Stochastic.py
+++ Python-MH-Local Search-2-opt-Stochastic.py
@@ -14,15 +14,84 @@
 # Required Libraries
 import pandas as pd
 import random
+import numpy  as np
 import copy
+from matplotlib import pyplot as plt 
 
-# Function: Distance
-def distance_calc(Xdata, route):
+# Function: Tour Distance
+def distance_calc(Xdata, city_tour):
     distance = 0
-    for k in range(0, len(route[0])-1):
+    for k in range(0, len(city_tour[0])-1):
         m = k + 1
-        distance = distance + Xdata.iloc[route[0][k]-1, route[0][m]-1]            
+        distance = distance + Xdata.iloc[city_tour[0][k]-1, city_tour[0][m]-1]            
     return distance
+
+# Function: Euclidean Distance 
+def euclidean_distance(x, y):       
+    distance = 0
+    for j in range(0, len(x)):
+        distance = (x.iloc[j] - y.iloc[j])**2 + distance   
+    return distance**(1/2) 
+
+# Function: Initial Seed
+def seed_function(Xdata):
+    seed = [[],float("inf")]
+    sequence = random.sample(list(range(1,Xdata.shape[0]+1)), Xdata.shape[0])
+    sequence.append(sequence[0])
+    seed[0] = sequence
+    seed[1] = distance_calc(Xdata, seed)
+    return seed
+
+# Function: Build Distance Matrix
+def buid_distance_matrix(coordinates):
+    Xdata = pd.DataFrame(np.zeros((coordinates.shape[0], coordinates.shape[0])))
+    for i in range(0, Xdata.shape[0]):
+        for j in range(0, Xdata.shape[1]):
+            if (i != j):
+                x = coordinates.iloc[i,:]
+                y = coordinates.iloc[j,:]
+                Xdata.iloc[i,j] = euclidean_distance(x, y)        
+    return Xdata
+
+# Function: Tour Plot
+def plot_tour_distance_matrix (Xdata, city_tour):
+    m = Xdata.copy(deep = True)
+    for i in range(0, Xdata.shape[0]):
+        for j in range(0, Xdata.shape[1]):
+            m.iloc[i,j] = (1/2)*(Xdata.iloc[0,j]**2 + Xdata.iloc[i,0]**2 - Xdata.iloc[i,j]**2)    
+    m = m.values
+    w, u = np.linalg.eig(np.matmul(m.T, m))
+    s = (np.diag(np.sort(w)[::-1]))**(1/2) 
+    coordinates = np.matmul(u, s**(1/2))
+    coordinates = coordinates.real[:,0:2]
+    xy = pd.DataFrame(np.zeros((len(city_tour[0]), 2)))
+    for i in range(0, len(city_tour[0])):
+        if (i < len(city_tour[0])):
+            xy.iloc[i, 0] = coordinates[city_tour[0][i]-1, 0]
+            xy.iloc[i, 1] = coordinates[city_tour[0][i]-1, 1]
+        else:
+            xy.iloc[i, 0] = coordinates[city_tour[0][0]-1, 0]
+            xy.iloc[i, 1] = coordinates[city_tour[0][0]-1, 1]
+    plt.plot(xy.iloc[:,0], xy.iloc[:,1], marker = 's', alpha = 1, markersize = 7, color = 'black')
+    plt.plot(xy.iloc[0,0], xy.iloc[0,1], marker = 's', alpha = 1, markersize = 7, color = 'red')
+    plt.plot(xy.iloc[1,0], xy.iloc[1,1], marker = 's', alpha = 1, markersize = 7, color = 'orange')
+    return
+
+# Function: Tour Plot
+def plot_tour_coordinates (coordinates, city_tour):
+    coordinates = coordinates.values
+    xy = pd.DataFrame(np.zeros((len(city_tour[0]), 2)))
+    for i in range(0, len(city_tour[0])):
+        if (i < len(city_tour[0])):
+            xy.iloc[i, 0] = coordinates[city_tour[0][i]-1, 0]
+            xy.iloc[i, 1] = coordinates[city_tour[0][i]-1, 1]
+        else:
+            xy.iloc[i, 0] = coordinates[city_tour[0][0]-1, 0]
+            xy.iloc[i, 1] = coordinates[city_tour[0][0]-1, 1]
+    plt.plot(xy.iloc[:,0], xy.iloc[:,1], marker = 's', alpha = 1, markersize = 7, color = 'black')
+    plt.plot(xy.iloc[0,0], xy.iloc[0,1], marker = 's', alpha = 1, markersize = 7, color = 'red')
+    plt.plot(xy.iloc[1,0], xy.iloc[1,1], marker = 's', alpha = 1, markersize = 7, color = 'orange')
+    return
 
 # Function: 2_opt Stochastic
 def local_search_2_opt_stochastic(Xdata, city_tour, recursive_seeding = 1):
@@ -45,12 +114,19 @@ def local_search_2_opt_stochastic(Xdata, city_tour, recursive_seeding = 1):
                         city_list[0][k] = best_route[0][k]          
                 best_route = copy.deepcopy(seed)
         count = count + 1  
-        print("Iteration = ", count, "->", city_list)
+        print("Iteration = ", count, "-> Distance =", city_list[1])
+    print(city_list)
     return city_list
 ######################## Part 1 - Usage ####################################
 
-X = pd.read_csv('Python-MH-Local Search-2-opt-Stochastic-Dataset-01.txt', sep = '\t') #17 cities => Optimum = 2085
+X = pd.read_csv('Python-MH-Local Search-2-opt-Stochastic-Dataset-01.txt', sep = '\t') # 17 cities = 1922.33
+seed = seed_function(X)
+ls2opts = local_search_2_opt_stochastic(X, city_tour = seed, recursive_seeding = 150)
+plot_tour_distance_matrix(X, ls2opts) # Red Point = Initial city; Orange Point = Second City # The generated coordinates (2D projection) are aproximated, depending on the data, the optimum tour may present crosses.
 
-cities = [[   1,  2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,   1   ], 4722]
-ls2opts = local_search_2_opt_stochastic(X, city_tour = cities, recursive_seeding = 100)
+Y = pd.read_csv('Python-MH-Local Search-2-opt-Stochastic-Dataset-02.txt', sep = '\t') # Berlin 52 = 7544.37
+X = buid_distance_matrix(Y)
+seed = seed_function(X)
+ls2opts = local_search_2_opt_stochastic(X, city_tour = seed, recursive_seeding = 550)
+plot_tour_coordinates (Y, ls2opts) # Red Point = Initial city; Orange Point = Second City
 
